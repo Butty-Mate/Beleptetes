c0e08b62936b541ede829d5b5fb6f5fdb0e17516
ButtyMate00 <118981147+ButtyMate00@users.noreply.github.com>
2025-02-26 21:54:19 +0100

    fix

---
+++ TT_BM_Belepteto_rendszer/WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace TT_BM_Belepteto_rendszer
+{
+    partial class Beléptető_Rendszer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(26, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Név:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(26, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(252, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Személyi igazolvány szám:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(26, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(242, 22);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Vezet-e az este folyamán:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(26, 190);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(275, 22);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Betöltötte már a 18. életévét:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(26, 234);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(383, 22);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Fog alkoholt fogasztani az este folyamán:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(26, 338);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(108, 22);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Stresszint:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F);
+            this.button1.Location = new System.Drawing.Point(553, 326);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(187, 84);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Belépés";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 13.25F);
+            this.textBox1.ForeColor = System.Drawing.Color.Black;
+            this.textBox1.Location = new System.Drawing.Point(83, 43);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(115, 28);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Text = "Gipsz Jakab";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 13.25F);
+            this.textBox2.ForeColor = System.Drawing.Color.Black;
+            this.textBox2.Location = new System.Drawing.Point(284, 90);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 28);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.Text = "123456BB";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F);
+            this.checkBox1.ForeColor = System.Drawing.Color.White;
+            this.checkBox1.Location = new System.Drawing.Point(274, 140);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(74, 26);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "Igen ";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F);
+            this.checkBox2.ForeColor = System.Drawing.Color.White;
+            this.checkBox2.Location = new System.Drawing.Point(340, 140);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(71, 26);
+            this.checkBox2.TabIndex = 10;
+            this.checkBox2.Text = "Nem";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F);
+            this.checkBox3.ForeColor = System.Drawing.Color.White;
+            this.checkBox3.Location = new System.Drawing.Point(305, 190);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(74, 26);
+            this.checkBox3.TabIndex = 11;
+            this.checkBox3.Text = "Igen ";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F);
+            this.checkBox4.ForeColor = System.Drawing.Color.White;
+            this.checkBox4.Location = new System.Drawing.Point(372, 190);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(71, 26);
+            this.checkBox4.TabIndex = 12;
+            this.checkBox4.Text = "Nem";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F);
+            this.checkBox5.ForeColor = System.Drawing.Color.White;
+            this.checkBox5.Location = new System.Drawing.Point(409, 234);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(74, 26);
+            this.checkBox5.TabIndex = 13;
+            this.checkBox5.Text = "Igen ";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F);
+            this.checkBox6.ForeColor = System.Drawing.Color.White;
+            this.checkBox6.Location = new System.Drawing.Point(475, 234);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(71, 26);
+            this.checkBox6.TabIndex = 14;
+            this.checkBox6.Text = "Nem";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 13.25F);
+            this.textBox3.ForeColor = System.Drawing.Color.Black;
+            this.textBox3.Location = new System.Drawing.Point(140, 336);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(28, 28);
+            this.textBox3.TabIndex = 15;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 14.25F);
+            this.button2.Location = new System.Drawing.Point(174, 336);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 28);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Mérés";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Beléptető_Rendszer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.checkBox6);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.Name = "Beléptető_Rendszer";
+            this.Text = "Beléptető Rendszer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ TT_BM_Belepteto_rendszer/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Drawing;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TT_BM_Belepteto_rendszer
+{
+    public partial class Beléptető_Rendszer : Form
+    {
+        public Beléptető_Rendszer()
+        {
+            InitializeComponent();
+            textBox1.Text = "Gipsz Jakab";
+            textBox2.Text = "123456BB";
+            textBox1.ForeColor = Color.Gray;
+            textBox2.ForeColor = Color.Gray;
+            textBox1.Enter += txtNev_Enter;
+            textBox1.Leave += txtNev_Leave;
+            textBox2.Enter += txtSzemelyiSzam_Enter;
+            textBox2.Leave += txtSzemelyiSzam_Leave;
+            textBox1.TextChanged += EllenorizMezoket;
+            textBox2.TextChanged += EllenorizMezoket;
+            checkBox1.CheckedChanged += CheckBox_CheckedChanged;
+            checkBox2.CheckedChanged += CheckBox_CheckedChanged;
+            checkBox3.CheckedChanged += CheckBox_CheckedChanged;
+            checkBox4.CheckedChanged += CheckBox_CheckedChanged;
+            checkBox5.CheckedChanged += CheckBox_CheckedChanged;
+            checkBox6.CheckedChanged += CheckBox_CheckedChanged;
+            button1.Enabled = false;
+        }
+
+        private void txtNev_Enter(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "Gipsz Jakab")
+            {
+                textBox1.Text = "";
+                textBox1.ForeColor = Color.Black;
+            }
+        }
+
+        private void txtNev_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                textBox1.Text = "Gipsz Jakab";
+                textBox1.ForeColor = Color.Gray;
+            }
+        }
+
+        private void txtSzemelyiSzam_Enter(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "123456BB")
+            {
+                textBox2.Text = "";
+                textBox2.ForeColor = Color.Black;
+            }
+        }
+
+        private void txtSzemelyiSzam_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox2.Text))
+            {
+                textBox2.Text = "123456BB";
+                textBox2.ForeColor = Color.Gray;
+            }
+        }
+
+        private void CheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            CheckBox clickedCheckBox = sender as CheckBox;
+
+
+            if (clickedCheckBox == checkBox1 && checkBox1.Checked)
+                checkBox2.Checked = false;
+            else if (clickedCheckBox == checkBox2 && checkBox2.Checked)
+                checkBox1.Checked = false;
+
+
+            if (clickedCheckBox == checkBox3 && checkBox3.Checked)
+                checkBox4.Checked = false;
+            else if (clickedCheckBox == checkBox4 && checkBox4.Checked)
+                checkBox3.Checked = false;
+
+            if (clickedCheckBox == checkBox5 && checkBox5.Checked)
+                checkBox6.Checked = false;
+            else if (clickedCheckBox == checkBox6 && checkBox6.Checked)
+                checkBox5.Checked = false;
+
+            EllenorizMezoket(sender, e);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (string.IsNullOrWhiteSpace(textBox3.Text))
+            {
+                MessageBox.Show("Kérjük, mérje meg a stresszszintet a belépés előtt!");
+                return;
+            }
+
+            string nev = textBox1.Text;
+            string szemelyiSzam = textBox2.Text;
+            bool vezet = checkBox1.Checked;
+            bool betoltotte18 = checkBox3.Checked;
+            bool iszik = checkBox5.Checked;
+
+            if (!betoltotte18 && iszik)
+            {
+                MessageBox.Show("Nem vagy elég idős az alkoholfogyasztáshoz.");
+                return;
+            }
+
+            if (vezet && iszik)
+            {
+                MessageBox.Show("Nem ihat alkoholt, ha vezet.");
+                return;
+            }
+
+            MessageBox.Show("Belépés engedélyezve.");
+            MentesFajlba(nev, szemelyiSzam, vezet, betoltotte18, iszik);
+            MezokTorlese();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Random veletlen = new Random();
+            int stresszint = veletlen.Next(0, 101);
+            textBox3.Text = stresszint.ToString();
+            textBox3.Visible = true;
+
+            if (stresszint > 60)
+            {
+                textBox3.ForeColor = Color.Red;
+            }
+            else
+            {
+                textBox3.ForeColor = Color.Green;
+            }
+        }
+
+        private void MentesFajlba(string nev, string szemelyiSzam, bool vezet, bool betoltotte18, bool iszik)
+        {
+
+            string nevKod = "10101";
+            string szemelyiSzamKod = "10010";
+            string vezetKod = "11011";
+            string betoltotte18Kod = "11100";
+            string iszikKod = "11111";
+            string gyanusSzemelyKod = "11101"; 
+
+
+            string nevBináris = SzovegToBináris(nev);
+            string szemelyiSzamBináris = SzovegToBináris(szemelyiSzam);
+
+
+            string vezetBináris = vezet ? "1" : "0";
+            string betoltotte18Bináris = betoltotte18 ? "1" : "0";
+            string iszikBináris = iszik ? "1" : "0";
+
+            bool gyanusSzemely = int.Parse(textBox3.Text) > 60;
+            string gyanusSzemelyBináris = gyanusSzemely ? "1" : "0";
+
+
+            /**
+            Adatstruktúra magyarázata:
+            -----------------
+            - Név: 10101 - A felhasználó neve bináris formában.
+            - Személyi igazolvány: 10010 - A személyi igazolvány száma bináris formában.
+            - Vezet: 11011 - Logikai érték: 1 (igen) vagy 0 (nem).
+            - Betöltötte a 18-at: 11100 - Logikai érték: 1 (igen) vagy 0 (nem).
+            - Iszik: 11111 - Logikai érték: 1 (igen) vagy 0 (nem).
+            - Gyanús személy: 11101 - Logikai érték: 1 (igen) vagy 0 (nem). Ha a stresszszint > 60, akkor 1, egyébként 0.
+            ---------------------
+            - A szöveges adatok karakterenként bináris formába kerülnek átalakításra.
+            - Minden adat külön sorba kerül, és személyenként két üres sor választja el az adatokat.
+            - Az új adatok a régi fájlhoz kerülnek hozzáadásra.
+             **/
+            string tartalom = $"{nevKod}{nevBináris}\n" +
+                             $"{szemelyiSzamKod}{szemelyiSzamBináris}\n" +
+                             $"{vezetKod}{vezetBináris}\n" +
+                             $"{betoltotte18Kod}{betoltotte18Bináris}\n" +
+                             $"{iszikKod}{iszikBináris}\n" +
+                             $"{gyanusSzemelyKod}{gyanusSzemelyBináris}\n\n\n"; 
+
+
+            File.AppendAllText("adat.txt", tartalom);
+        }
+
+        private void MezokTorlese()
+        {
+            textBox1.Text = "Gipsz Jakab";
+            textBox2.Text = "123456BB";
+            textBox1.ForeColor = Color.Gray;
+            textBox2.ForeColor = Color.Gray;
+            checkBox1.Checked = false;
+            checkBox2.Checked = false;
+            checkBox3.Checked = false;
+            checkBox4.Checked = false;
+            checkBox5.Checked = false;
+            checkBox6.Checked = false;
+            textBox3.Text = " ";
+            textBox3.Visible = false;
+        }
+
+        private void EllenorizMezoket(object sender, EventArgs e)
+        {
+
+            bool nevHelyes = textBox1.Text != "Gipsz Jakab" &&
+                            !string.IsNullOrWhiteSpace(textBox1.Text) &&
+                            textBox1.Text.Contains(" ") &&
+                            textBox1.Text.Trim().Split(' ').Length >= 2;
+            bool szemelyiSzamHelyes = Regex.IsMatch(textBox2.Text, @"^\d{6}[A-Za-z]{2}$");
+
+
+            bool vezetHelyes = checkBox1.Checked || checkBox2.Checked;
+            bool betoltotte18Helyes = checkBox3.Checked || checkBox4.Checked;
+            bool iszikHelyes = checkBox5.Checked || checkBox6.Checked;
+
+
+            bool iszikEngedelyezett = (!checkBox5.Checked || (checkBox3.Checked && !checkBox1.Checked));
+
+            button1.Enabled = nevHelyes && szemelyiSzamHelyes && vezetHelyes && betoltotte18Helyes && iszikHelyes && iszikEngedelyezett;
+        }
+
+        private string SzovegToBináris(string szoveg)
+        {
+            string bináris = "";
+            foreach (char karakter in szoveg)
+            {
+                bináris += Convert.ToString(karakter, 2).PadLeft(8, '0');
+            }
+            return bináris.Trim();
+        }
+    }
+}
